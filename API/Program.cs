3a1679e11d572d7983fa76ee9b4644ad80fb46fc
nebHailemariam <nebhailemariam@gmail.com>
2022-05-25 14:53:17 +0300

    refactor: disable swagger in production

--- API/Program.cs
+++ API/Program.cs
@@ -159,15 +159,15 @@ builder.Services.AddAuthorization(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-app.UseSwagger();
-app.UseSwaggerUI(c =>
+if (app.Environment.IsDevelopment())
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
-    c.RoutePrefix = string.Empty;
-});
-// }
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
 
 // Add global error handler.
 app.UseMiddleware<ErrorHandlerMiddleware>();
